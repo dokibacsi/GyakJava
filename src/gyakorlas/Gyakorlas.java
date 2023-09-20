2bfb13cacbe734742d518aa0a885c214aa0b7438
dokibacsi <kvalcsi02@gmail.com>
2023-09-20 12:52:36 +0200

    elag

--- src/gyakorlas/Gyakorlas.java
+++ src/gyakorlas/Gyakorlas.java
@@ -1,8 +1,11 @@
 package gyakorlas;
 public class Gyakorlas {
+    int x;
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            
+        }
+        if(x == 0){
+            int h;
         }
     }
     
