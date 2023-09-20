4d645204448c9ba82b1fed563f2be083e18ca935
dokibacsi <kvalcsi02@gmail.com>
2023-09-20 12:58:30 +0200

    noStaticMethod

--- src/gyakorlas/Gyakorlas.java
+++ src/gyakorlas/Gyakorlas.java
@@ -4,9 +4,13 @@ public class Gyakorlas {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
         }
-        if(x == 0){
-            int h;
+        if(true){
+            metodus();
         }
+    }
+    
+    private void metodus(){
+        System.out.println(":)");
     }
     
 }
